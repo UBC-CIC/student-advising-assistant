261be6665aa0234437fae4bfc691762451769338
astevins <arya.stevinson@gmail.com>
2023-07-17 09:14:53 -0700

    Fix fastchat adapter

--- flask_app/fastchat_adapter.py
+++ flask_app/fastchat_adapter.py
@@ -1,23 +1,24 @@
-from langchain.llms import BaseLLM
+from langchain.llms.base import BaseLLM, LLM
 from langchain.callbacks.manager import CallbackManagerForLLMRun
 from fastchat.model import get_conversation_template
 from typing import Optional, List, Any
 
-class FastChatLLM(BaseLLM):
+class FastChatLLM(LLM):
     # Adapter to use any supported LLM with fastchat prompting tools
     # Adapted from https://github.com/lm-sys/FastChat/blob/main/fastchat/serve/huggingface_api.py
 
     base_llm: BaseLLM
     template: Any
+    llm_type: str = 'fastchat-adapter'
     
-    def __init__(self, base_llm: BaseLLM, model_name: str):
+    @classmethod
+    def from_base_llm(cls, base_llm: BaseLLM, model_name: str):
         """
         - base_llm: The langchain LLM model that will be called
-        - model_name: The name of the model, used to get conversation templates from fastchat
+        - model_name: The name of the base llm, used to get conversation templates from fastchat
              See https://github.com/lm-sys/FastChat/blob/main/fastchat/conversation.py for options
         """
-        self.base_llm = base_llm
-        self.template = get_conversation_template(model_name)
+        return cls(base_llm=base_llm, template=get_conversation_template(model_name))
 
     def _call(
         self,
@@ -31,5 +32,15 @@ class FastChatLLM(BaseLLM):
         self.template.append_message(self.template.roles[0], prompt)
         self.template.append_message(self.template.roles[1], None)
         fastchat_prompt = self.template.get_prompt()
-        result = self.base_llm.generate(fastchat_prompt, stop, run_manager, **kwargs)
-        return result
+        result = self.base_llm.generate([fastchat_prompt], stop, run_manager, **kwargs)
+        
+        output = result.generations[0][0].text
+        if output.startswith(fastchat_prompt): 
+            # If the model returned the prompt as well as generated text, remove the prompt
+            output = output[len(fastchat_prompt):]
+            
+        return output
+    
+    def _llm_type(self) -> str:
+        """Return type of llm."""
+        return self.llm_type
