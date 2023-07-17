4a7cf1adf0a2a17b7bbf0d19bacf82b08cc1ac23
astevins <arya.stevinson@gmail.com>
2023-07-17 15:20:56 -0700

    Fix merge conflicts

---
+++ backend/cdk/lib/vpc-stack.ts
@@ -0,0 +1,108 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { ManagedPolicy } from "aws-cdk-lib/aws-iam";
+import { NatProvider } from "aws-cdk-lib/aws-ec2";
+
+export class VpcStack extends Stack {
+  public readonly vpc: ec2.Vpc;
+  public readonly cidr: string;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const natGatewayProvider = ec2.NatProvider.gateway();
+  
+    // VPC for application
+    this.cidr = '10.0.0.0/16'
+    this.vpc = new ec2.Vpc(this, "Vpc", {
+      //cidr: "10.0.0.0/16",
+      ipAddresses: ec2.IpAddresses.cidr(this.cidr),
+      natGatewayProvider: natGatewayProvider,
+      natGateways: 1,
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: "public-subnet-1",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          name: "isolated-subnet-1",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+      gatewayEndpoints: {
+        S3: {
+          service: ec2.GatewayVpcEndpointAwsService.S3,
+        },
+      },
+    });
+    this.vpc.addFlowLog("vpcFlowLog");
+
+    // Get default security group for VPC
+    const defaultSecurityGroup = ec2.SecurityGroup.fromSecurityGroupId(
+      this,
+      id,
+      this.vpc.vpcDefaultSecurityGroup
+    );
+
+    // Add SSM endpoint to VPC
+    this.vpc.addInterfaceEndpoint("SSM Endpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.SSM,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    // Add secrets manager endpoint to VPC
+    this.vpc.addInterfaceEndpoint("Secrets Manager Endpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    // Add Glue endpoint to VPC
+    this.vpc.addInterfaceEndpoint("Glue Endpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.GLUE,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    // Add Cloudwatch endpoint to VPC
+    this.vpc.addInterfaceEndpoint("Cloudwatch Endpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    // Add ECR endpoint to VPC
+    this.vpc.addInterfaceEndpoint("ECR Endpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    // Add ECR Docker endpoint to VPC
+    this.vpc.addInterfaceEndpoint("ECR Docker Endpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    // // Sagemaker Endpoints for VPC
+    // this.vpc.addInterfaceEndpoint("Sagemaker-API-Endpoint", {
+    //   service: ec2.InterfaceVpcEndpointAwsService.SAGEMAKER_API,
+    //   subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    // });
+
+    // this.vpc.addInterfaceEndpoint("Sagemaker-Runtime-Endpoint", {
+    //   service: ec2.InterfaceVpcEndpointAwsService.SAGEMAKER_RUNTIME,
+    //   subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    // });
+
+    this.vpc.isolatedSubnets.forEach(
+      ({ routeTable: { routeTableId } }, index) => {
+        new ec2.CfnRoute(this, "PrivateSubnetPeeringConnectionRoute" + index, {
+          destinationCidrBlock: "0.0.0.0/0",
+          routeTableId,
+          natGatewayId: natGatewayProvider.configuredGateways[0].gatewayId,
+        });
+      }
+    );
+  }
+}
--- document_scraping/process_site_dumps.py
+++ document_scraping/process_site_dumps.py
@@ -399,14 +399,14 @@ specialization_suffix_regex = f".+({'|'.join(specialization_suffixes)})"
 specialization_regex = re.compile(f"^({specialization_prefix_regex}|{specialization_suffix_regex})$")
 
 def calendar_extract_metadata(url: str, titles: List[str], parent_titles: List[str], text: str):
-    metadata = {}
+    metadata = {'specialization': []}
     for subtitle in parent_titles + titles:
         if 'faculty' not in metadata and re.match(faculty_regex, subtitle):
             metadata['faculty'] = subtitle
         if 'program' not in metadata and re.match(program_regex, subtitle):
             metadata['program'] = subtitle
-        if 'specialization' not in metadata and re.match(specialization_regex, subtitle):
-            metadata['specialization'] = subtitle
+        if re.match(specialization_regex, subtitle):
+            metadata['specialization'].append(subtitle)
 
     if re.search('\* \d+ credits of ', text):
         if 'specialization' in metadata:
@@ -416,10 +416,10 @@ def calendar_extract_metadata(url: str, titles: List[str], parent_titles: List[s
     return metadata
 
 def blog_extract_metadata(url: str, titles: List[str], parent_titles: List[str], text: str):
-    metadata = {'faculty': 'The Faculty of Science', 'program': 'Bachelor of Science'}
+    metadata = {'faculty': 'The Faculty of Science', 'program': 'Bachelor of Science', 'specialization': []}
     for subtitle in parent_titles + titles:
-        if 'specialization' not in metadata and re.match(specialization_regex, subtitle):
-            metadata['specialization'] = subtitle
+        if re.match(specialization_regex, subtitle):
+            metadata['specialization'].append(subtitle)
     return metadata
 
 calendar_is_new_format = True
