2d3dea6184c5af0d3b62c57c23d1b7be806351e6
astevins <arya.stevinson@gmail.com>
2023-07-19 08:51:05 -0700

    Swap from environment variables to AWS parameter store for document scraping

--- document_scraping/tools.py
+++ document_scraping/tools.py
@@ -19,21 +19,3 @@ def write_file(writer: Callable):
             print(f"Unable to write to file: {str(e)}")
             print("Could not save the files. Make sure the files to write are not open.")
             response = input("Try saving files again? <y,n> ")
-
-
-def upload_files(directory: str, bucket_name: str, s3_client):
-
-    """
-    Arguments:
-        directory: the directory that will be recursively upload
-        bucketname: the name of the s3 bucket to upload
-        s3_client: the S3 client generated with boto3.client("s3")
-    """
-    
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            local_path = os.path.join(root, file)
-            relative_path = os.path.relpath(local_path, directory)
-            s3_key = os.path.join(directory, os.path.dirname(relative_path), file)
-            s3_client.upload_file(local_path, bucket_name, s3_key)
-            print(f"Uploaded {local_path} to S3 bucket {bucket_name} with key {s3_key}")
