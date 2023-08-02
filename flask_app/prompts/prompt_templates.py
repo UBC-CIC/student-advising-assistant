83d5d8d9180b622aa822b667a7ed22b48bddd43c
astevins <arya.stevinson@gmail.com>
2023-08-02 14:54:51 -0700

    Update prompts for vicuna 1.5

--- flask_app/prompts/prompt_templates.py
+++ flask_app/prompts/prompt_templates.py
@@ -43,8 +43,7 @@ The assistant follows instructions precisely and gives concise answers."""
 
 fastchat_system_detailed = """
 A chat between a University of British Columbia (UBC) student and an artificial intelligence assistant. 
-The assistant gives helpful, detailed, and polite answers to the user's questions.
-The assistant only uses the context provided in each message to answer questions, and no other information."""
+The assistant gives helpful, detailed, and polite answers to the user's questions."""
 
 ### FILTER PROMPTS
 
@@ -135,15 +134,16 @@ def vicuna_filter_question_str(program_info: Dict, topic: str, query: str) -> st
     context_str = filter_context_str(program_info, topic)
     
     if context_str:
-        question_str += f'If the document is not about {context_str}, say no.'
+        question_str += f'If the context is not about {context_str}, then say no and explain why the context is not relevant.'
         
     question_str += f'\n> Question: {query}\n'
     return question_str
 
 vicuna_filter_template = """
-Does the document below contain information that helps answer the following question?
+Does the context below contain information that answers the following question?
+Only use information directly present in the context.
 {question}
-> Document: {context}
+> Context: {context}
 """
 vicuna_filter_prompt = PromptTemplate(template=vicuna_filter_template, input_variables=["question","context"], output_parser=VerboseFlexibleBooleanOutputParser())
 
@@ -155,8 +155,9 @@ default_qa_prompt = PromptTemplate(template=default_qa_template, input_variables
 
 # Template for Vicuna question answering
 vicuna_qa_template = """
-    Please answer the question based on the context below. Only use information present in the context. If you don't have
-    enough information to answer, say 'There is not enough information to answer'.\n
+    Please answer the question based on the context below.
+    Use only the context provided to answer the question, and no other information.
+    If the context doesn't have enough information to answer, explain what information is missing.\n
     Context:{context}\n
     Question:{question}""".strip()
 vicuna_qa_prompt = PromptTemplate(template=vicuna_qa_template, input_variables=["context","question"])
@@ -168,8 +169,9 @@ huggingface_qa_prompt = PromptTemplate(template=huggingface_qa_template, input_v
 ### OTHER PROMPTS
 
 spelling_correction_template = """
-Please correct the grammar and spelling errors in the following text. Do not respond to the question, only return the corrected version of the text.
-
+Please correct the grammar and spelling errors in the following text. 
+Return only the corrected version of the text, do not respond to the question and do not include any annotation.
+>>> Input:
 {text}
 """
 spelling_correction_prompt = PromptTemplate(template=spelling_correction_template, input_variables=["text"])
