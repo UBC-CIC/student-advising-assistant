5f9f788ec9356a031a9bbce5dc0f92e4b5ed9f0c
astevins <arya.stevinson@gmail.com>
2023-08-25 10:50:21 -0700

    Support relevance threshold for pgvector

--- flask_app/retrievers/pgvector_retriever.py
+++ flask_app/retrievers/pgvector_retriever.py
@@ -1,13 +1,75 @@
 from langchain.vectorstores.base import VectorStoreRetriever
 from langchain.schema import Document
-from langchain.vectorstores.pgvector import PGVector
-from typing import List, Dict, Tuple
+from langchain.vectorstores.pgvector import PGVector, DistanceStrategy
+from typing import List, Dict, Tuple, Callable, Optional
 import os
 import copy
 import ast
 from .tools import load_json_file
 from .base import Retriever, INDEX_PATH
 
+class MyPGVectorRetriever(PGVector):
+    """
+    Temporary fix since the current Langchain PGVector class has a bug so that it does not work
+    with similarity score thresholds.
+    https://github.com/langchain-ai/langchain/blob/v0.0.235/langchain/vectorstores/pgvector.py#L582
+    """
+    
+    def similarity_search_with_score(
+        self,
+        query: str,
+        k: int = 4,
+        filter: Optional[dict] = None,
+        **kwargs
+    ) -> List[Tuple[Document, float]]:
+        """
+        Add kwargs to support similarity search with threshold, since the threshold
+        is a kwarg used by functions upstream
+        """
+        return super().similarity_search_with_score(query,k,filter)
+    
+    def similarity_search_with_score_by_vector(
+        self,
+        embedding: List[float],
+        k: int = 4,
+        filter: Optional[dict] = None,
+        **kwargs
+    ) -> List[Tuple[Document, float]]:
+        """
+        Add kwargs to support similarity search with threshold, since the threshold
+        is a kwarg used by functions upstream
+        """
+        result = super().similarity_search_with_score_by_vector(embedding,k,filter)
+        print([score for doc, score in result])
+        return result
+    
+    def _select_relevance_score_fn(self) -> Callable[[float], float]:
+        """
+        The 'correct' relevance function
+        may differ depending on a few things, including:
+        - the distance / similarity metric used by the VectorStore
+        - the scale of your embeddings (OpenAI's are unit normed. Many others are not!)
+        - embedding dimensionality
+        - etc.
+        """
+        if self.override_relevance_score_fn is not None:
+            return self.override_relevance_score_fn
+
+        # Default strategy is to rely on distance strategy provided
+        # in vectorstore constructor
+        if self._distance_strategy == DistanceStrategy.COSINE:
+            return self._cosine_relevance_score_fn
+        elif self._distance_strategy == DistanceStrategy.EUCLIDEAN:
+            return self._euclidean_relevance_score_fn
+        elif self._distance_strategy == DistanceStrategy.MAX_INNER_PRODUCT:
+            return self._max_inner_product_relevance_score_fn
+        else:
+            raise ValueError(
+                "No supported normalization function"
+                f" for distance_strategy of {self.distance_strategy}."
+                "Consider providing relevance_score_fn to PGVector constructor."
+            )
+    
 class PGVectorRetriever(Retriever):
     index_type: str = 'pgvector'
     index_config_path: str = os.path.join(INDEX_PATH, index_type, 'index_config.json')
@@ -34,9 +96,10 @@ class PGVectorRetriever(Retriever):
         self.num_embed_concats = len(index_config['embeddings'])
         
         # Connect to the pgvector db
-        db = PGVector.from_existing_index(embeddings_model, index_config['name'], connection_string=connection_string)
+        db = MyPGVectorRetriever.from_existing_index(embeddings_model, index_config['name'], connection_string=connection_string)
+        
         self.retriever = VectorStoreRetriever(vectorstore=db)
-    
+            
     def semantic_search(self, filter: Dict, program_info: Dict, topic: str, query: str, k = 5, threshold = 0) -> List[Document]:
         """
         Return the documents from similarity search with the given context and query
@@ -88,7 +151,7 @@ class PGVectorRetriever(Retriever):
         """
         self.retriever.search_kwargs['filter'] = filter
         
-        query_str = ' : '.join(list(program_info.values()) + [topic,query])
+        query_str = ' : '.join([value for value in list(program_info.values()) + [topic,query] if len(value) > 0])
         return self._retriever_combined_query(query_str), {}
     
     def _response_converter(self, response: List[Document]) -> List[Document]:
