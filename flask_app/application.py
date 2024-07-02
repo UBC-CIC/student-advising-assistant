1af41b0f69e6ce52c8a81deec44f8dac68b6e519
Aman <apraka01@student.ubc.ca>
2024-07-02 10:42:19 -0700

    Changed back-end to use Bedrock and new initialize logic. Answer endpoint returns JSON for now.

--- flask_app/application.py
+++ flask_app/application.py
@@ -15,9 +15,12 @@ sys.path.append('..')
 from flask import Flask, request, render_template, Response
 import json
 import os 
+import numpy as np
 from typing import List
 from importlib import reload 
 from aws_helpers.rds_tools import execute_and_fetch
+from langchain_community.embeddings.bedrock import BedrockEmbeddings
+from langchain_aws import BedrockLLM
 
 ### Constants
 FACULTIES_PATH = os.path.join('data','documents','faculties.json')
@@ -25,12 +28,18 @@ TITLE_PATH = os.path.join('static','app_title.txt')
 DEFAULTS_PATH = os.path.join('static','defaults.json')
 DEV_MODE = 'MODE' in os.environ and os.environ.get('MODE') == 'dev'
 
+# Defining Constants for LLMs
+LLAMA_3_8B = "meta.llama3-8b-instruct-v1:0"
+LLAMA_3_70B = "meta.llama3-70b-instruct-v1:0"
+MISTRAL_7B = "mistral.mistral-7b-instruct-v0:2"
+MISTRAL_LARGE = "mistral.mistral-large-2402-v1:0"
+
 ### Globals (set upon load)
 application = Flask(__name__)
 app_title = None
 faculties = {}
 last_updated_time = None
-langchain_inference_module = None
+initialize_module = None
 store_feedback_module = None
 
 # Helper functions
@@ -67,10 +76,121 @@ def get_last_updated_time():
         LIMIT 1"""
     result = execute_and_fetch(sql, dev_mode=DEV_MODE)
     return result[0][0].strftime("%m/%d/%Y, %H:%M:%S (UTC)")
+
+# Format all texts in the doc as one string when we pass prompt to LLM
+def format_docs(docs):
+    formatted_docs = "\n".join([f"Document {idx}:\n{doc['text']}" for idx, doc in enumerate(docs, 1)])
+    return formatted_docs
+
+# Get most similar documents from the database
+def get_docs(query_embedding, number):
+    embedding_array = np.array(query_embedding)
+
+    # Get RDS connection
+    try:
+        conn = initialize_module.return_connection()
+        curr = conn.cursor()
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return []
+    
+    top_docs = []
+    try:
+        cur = conn.cursor()
+        # Get the top N most similar documents using the KNN <=> operator
+        cur.execute(f"SELECT url, text FROM test_embeddings ORDER BY embedding <=> %s LIMIT {number}", (embedding_array,))
+        results = cur.fetchall()
+        # Each item in list will be a dictionary with key values 'url' and 'text'
+        for result in results:
+            doc_dict = {"url": result[0], "text": result[1]}
+            top_docs.append(doc_dict)
+        cur.close()
+    except Exception as e:
+        print(f"Error when retrieving! {str(e)}")
+        conn.rollback()
+    finally:
+        cur.close()
+    return top_docs
+
+def check_if_documents_relates(docs, user_prompt, llm):
+
+    system_prompt = """You are tasked with determining if the document helps answer the question. 
+                        Either provide an answer saying "Yes, ..." with a short explaination 
+                        or "No, ..." with a short explaination based on your analysis.
+                        Avoid any unrelated information or questions."""
+
+    doc_relates = []
+    for doc in docs:
+        if llm.model_id == LLAMA_3_8B or llm.model_id == LLAMA_3_70B:
+            prompt = f"""
+                <|begin_of_text|>
+                <|start_header_id|>system<|end_header_id|>
+                {system_prompt}
+                <|eot_id|>
+                <|start_header_id|>question<|end_header_id|>
+                {user_prompt}
+                <|eot_id|>
+                <|start_header_id|>document<|end_header_id|>
+                {doc['text']}
+                <|eot_id|>
+                <|start_header_id|>assistant<|end_header_id|>
+                """
+        else:
+            prompt = f"""Here is a queston that a user asked: {user_prompt}.
+                Here is the text from a document: {doc['text']}.
+                {system_prompt}
+                """
+        response = llm.invoke(prompt)
+        doc_relates_dict = {"url": doc['url'], "text": doc['text'], "relate": response}
+        doc_relates.append(doc_relates_dict)
+
+    return doc_relates
+
+def answer_prompt(user_prompt, number_of_docs):
+
+    # Initialize the Bedrock Embeddings model
+    embeddings = BedrockEmbeddings()
+
+    docs = get_docs(embeddings.embed_query(user_prompt), number_of_docs)
+
+    documents = format_docs(docs)
+
+    # Get the LLM we want to invoke
+    llm = BedrockLLM(
+                        model_id=LLAMA_3_8B
+                    )
+
+    system_prompt = "You are a helpful UBC student advising assistant who answers with kindness while being concise. Only generate one human readable answer"
+
+    if llm.model_id == LLAMA_3_8B or llm.model_id == LLAMA_3_70B:
+        prompt = f"""
+            <|begin_of_text|>
+            <|start_header_id|>system<|end_header_id|>
+            {system_prompt}
+            <|eot_id|>
+            <|start_header_id|>user<|end_header_id|>
+            {user_prompt}
+            <|eot_id|>
+            <|start_header_id|>documents<|end_header_id|>
+            {documents}
+            <|eot_id|>
+            <|start_header_id|>assistant<|end_header_id|>
+            """
+    else:
+        prompt = f"""{system_prompt}.
+            Here is the question: {user_prompt}.
+            Here are the source documents: {documents}
+            """
+
+    answer = llm.invoke(prompt)
+
+    check_docs = check_if_documents_relates(docs, user_prompt, llm)
+
+    return {"answer": answer, "check_docs": check_docs}
         
 @application.route('/', methods=['GET'])
 def home():
-    if not langchain_inference_module:
+    if not initialize_module:
         # App is not yet initialized
         return render_template('not_initialized.html')
     
@@ -79,7 +199,7 @@ def home():
 
 @application.route('/answer', methods=['POST'])
 async def answer():
-    if not langchain_inference_module:
+    if not initialize_module:
         # App is not yet initialized
         return render_template('not_initialized.html',title=app_title)
     
@@ -89,25 +209,40 @@ async def answer():
     filter_elems = ['faculty','program','specialization','year']
     program_info = {filter_elem: request.form[filter_elem] for filter_elem in filter_elems}
     
-    # Checks if the form was submitted with a starting document id
-    start_doc = request.args.get('doc')
-    if start_doc:
-        start_doc = int(start_doc)
+    formatted_question = ""
+    if program_info['faculty']:
+        formatted_question += f"I am in {program_info['faculty']}. "
+    
+    if program_info['program']:
+        formatted_question += f"I am in the {program_info['program']} program. "
+    
+    if program_info['specialization']:
+        formatted_question += f"I am in the {program_info['specialization']} specialization. "
+    
+    if program_info['year']:
+        formatted_question += f"I am in my {program_info['year']}. "
+    
+    if topic:
+        formatted_question += f"The topic of the question is {topic}. "
+    
+    formatted_question += question
 
-    # Run the model inference
-    config = {
-        'start_doc': start_doc
+    response = answer_prompt(formatted_question, 5)
+        
+    # Neatly format the response
+    formatted_response = {
+        "answer": response["answer"],
+        "related_documents": []
     }
-    docs, main_response, alerts, removed_docs = await langchain_inference_module.run_chain(program_info,topic,question,config)
-    
-    # Log the question
-    context_str = ' : '.join([value for value in list(program_info.values()) + [topic] if len(value) > 0])
-    log_question(question, context_str, main_response, [doc.metadata['doc_id'] for doc in docs])
-    
-    # Render the results
-    return render_template('ans.html',title=app_title,question=question,context=context_str,docs=docs,
-                           form=request.form.to_dict(), main_response=main_response, alerts=alerts,
-                           removed_docs=removed_docs, last_updated=last_updated_time)
+
+    for doc in response["check_docs"]:
+        formatted_response["related_documents"].append({
+            "url": doc["url"],
+            "text": doc["text"],
+            "related": doc["relate"]
+        })
+
+    return json.dumps(formatted_response, indent=4)
 
 @application.route('/feedback', methods=['POST'])
 async def feedback():
@@ -134,13 +269,13 @@ def initialize():
     Imports files and runs all initial setup of the app
     Exists as an endpoint so that configuration can be reloaded on demand
     """
-    global langchain_inference_module, feedback_module
+    global initialize_module, feedback_module
     
-    if not langchain_inference_module:
-        import langchain_inference as langchain_inference_module
+    if not initialize_module:
+        import initialize as initialize_module
         import feedback as feedback_module
     else:
-        reload(langchain_inference_module)
+        reload(initialize_module)
         reload(feedback_module)
     
     # Upon loading, load the available settings for the form
---
+++ flask_app/initialize.py
@@ -0,0 +1,63 @@
+import os
+import psycopg2
+from pgvector.psycopg2 import register_vector
+from aws_helpers.param_manager import get_param_manager
+from aws_helpers.s3_tools import download_s3_directory
+
+# If process is running locally, activate dev mode
+DEV_MODE = 'MODE' in os.environ and os.environ.get('MODE') == 'dev'
+
+### LOAD AWS CONFIG
+param_manager = get_param_manager()
+
+# Set variable to represent connection to RDS
+connection = None
+
+def download_all_dirs():
+    """
+    Downloads the directories from s3 necessary for the flask app
+    """
+    # Specify directories to download
+    dirs = ['documents']
+        
+    for dir in dirs:
+        download_s3_directory(dir, output_prefix='data')
+        
+download_all_dirs()
+
+# Get RDS secrets to get connection parameters
+db_secret = param_manager.get_secret("credentials/RDSCredentials")
+
+# Define the connection parameters
+connection_params = {
+    'dbname': db_secret["dbname"],
+    'user': db_secret["username"],
+    'password': db_secret["password"],
+    'host': db_secret["host"],
+    'port': db_secret["port"]
+}
+
+# Create the connection string
+connection_string = " ".join([f"{key}={value}" for key, value in connection_params.items()])
+
+try:
+    # Connect to PostgreSQL database using connection string
+    connection = psycopg2.connect(connection_string)
+    cur = connection.cursor()
+    print("Connected to RDS instance!")
+
+    # Register pgvector extension
+    register_vector(connection)
+except:
+    print("Error connecting to RDS instance!")
+
+def return_connection():
+    global connection
+    if connection is None or connection.closed:
+        try:
+            connection = psycopg2.connect(connection_string)
+            register_vector(connection)
+        except Exception as e:
+            print(f"Error reconnecting to RDS instance: {e}")
+            connection = None
+    return connection
